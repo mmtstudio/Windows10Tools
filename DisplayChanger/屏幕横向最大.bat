4478b248c4417a304d3ccf1f65f7cf172673faed
Studio Mmt <mmtstudio@outlook.com>
2020-08-11 22:32:08 +0800

    DisplayChanger

---
+++ DisplayChanger/屏幕横向最大.bat
@@ -0,0 +1,3 @@
+start "" "C:\Program Files (x86)\12noon Display Changer\dc64.exe" -rotate=up
+sleep 1
+start "" "C:\Program Files (x86)\12noon Display Changer\dc64.exe" -max
---
+++ DisplayChanger/屏幕纵向864x1152.bat
@@ -0,0 +1,3 @@
+start "" "C:\Program Files (x86)\12noon Display Changer\dc64.exe" -rotate=left
+sleep 1
+start "" "C:\Program Files (x86)\12noon Display Changer\dc64.exe" -width=864 -height=1152
